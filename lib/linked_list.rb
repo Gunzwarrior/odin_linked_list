6a397518d2da0f38c6475b0db2536ce52b3a2e28
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-12-28 10:17:14 +0100

    Work on append method

---
+++ lib/linked_list.rb
@@ -0,0 +1,24 @@
+require_relative "node"
+
+class LinkedList
+  attr_accessor :head, :tail
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    if head.nil?
+      @head = Node.new(value)
+      @tail = @head
+      p head
+      puts "node was added as head"
+    else
+      pointer = head
+      until pointer.next_node?.nil?
+        pointer = pointer.next_node?
+      end
+      pointer.next_node = Node.new(value)
+    end
+  end
+end
---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+require_relative 'linked_list'
+
+list = LinkedList.new
+p list
+list.append("X")
+p list
